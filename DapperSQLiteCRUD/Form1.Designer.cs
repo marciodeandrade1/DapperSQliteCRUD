f68d666299accf4e3fe71285fff0ba2ff362b970
Marcio de Andrade <marcio@requestdev.net>
2024-04-22 17:42:39 -0400

    Adicionar arquivos de projeto.

---
+++ DapperSQLiteCRUD/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DapperSQLiteCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateDatabaseTable = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateDatabaseTable
+            // 
+            this.btnCreateDatabaseTable.Location = new System.Drawing.Point(45, 29);
+            this.btnCreateDatabaseTable.Name = "btnCreateDatabaseTable";
+            this.btnCreateDatabaseTable.Size = new System.Drawing.Size(192, 23);
+            this.btnCreateDatabaseTable.TabIndex = 0;
+            this.btnCreateDatabaseTable.Text = "Create Database Table";
+            this.btnCreateDatabaseTable.UseVisualStyleBackColor = true;
+            this.btnCreateDatabaseTable.Click += new System.EventHandler(this.btnCreateDatabaseTable_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(45, 161);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(289, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(391, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(289, 262);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(370, 262);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 342);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateDatabaseTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateDatabaseTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ DapperSQLiteCRUD/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DapperSQLiteCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCreateDatabaseTable_Click(object sender, EventArgs e)
+        {
+            using (IDbConnection db = GetConnection())
+            {
+                db.Execute("Create Table Students(ID int, Name varchar(50))");
+                MessageBox.Show("Database e table created");
+            }
+        }
+        public IDbConnection GetConnection()
+            {
+                return new SQLiteConnection(@"Data Source=D:\StudentDB.db; Version=3; New=true");
+            }
+        public class Student
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!File.Exists(@"D:\\StudentDB.db"))
+            {
+                LoadData();
+            }
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (IDbConnection db = GetConnection())
+            {
+                var list = db.Query<Student>("select * from Students").ToList();
+                if (list.Count() > 0)
+                {
+                    dataGridView1.DataSource = list;
+                }
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (IDbConnection db = GetConnection())
+            {
+                int id = Convert.ToInt32(textBox1.Text);
+                string name = textBox2.Text;
+
+              int n =  db.Execute("insert into students(id, name) values(@ID, @Name)", new { ID = id, Name = name });
+                if (n > 0)
+                {
+                    MessageBox.Show("inserted");
+                    LoadData();
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = (int) dataGridView1.CurrentRow.Cells[0].Value;
+            using (IDbConnection db = GetConnection())
+            {
+               int n = db.Execute("delete from students where id=@ID", new { ID = id });
+                if (n > 0)
+                {
+                    MessageBox.Show("deleted");
+                    LoadData();
+                }
+
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            string name = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+            using (IDbConnection db = GetConnection())
+            {
+                int n = db.Execute("update students set name=@Name where id=@ID", new { Name=name, ID = id });
+                if (n > 0)
+                {
+                    MessageBox.Show("updated");
+                    LoadData();
+                }
+
+            }
+        }
+    }
+}
